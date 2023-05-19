764ca905fcdf9d6e15e32e9c96bf94fa066c537b
Sampath Lonka <48767274+sampathlonka@users.noreply.github.com>
2023-05-19 15:22:09 +0000

    created openAPI key setup

--- sam-web/src/pages/index.js
+++ sam-web/src/pages/index.js
@@ -1,51 +1,45 @@
-
 import { useState } from "react";
 
-const ChatBox = () => {
-  const [messages, setMessages] = useState([]);
+export default function Home() {
+  const [apiKey, setApikey] = useState('');
+  const API_URL = "https://api.openai.com/v1/chat/completions"
 
-  const handleMessageSend = () => {
-    // Add logic to send message
-  };
+  async function sendRequest() {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Beareer ' + apiKey,
+      },
+      body: JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "Hello!"}]
+      }),
+    });
 
-  return (
-    <div className="flex flex-col h-full">
-      <div className="overflow-auto flex-grow">
-        {/* Render chat messages here */}
-        {messages.map((message, index) => (
-          <div key={index} className="flex justify-end mb-2">
-            <div className="bg-blue-500 text-white py-2 px-4 rounded-lg max-w-3/4">
-              {message}
-            </div>
-          </div>
-        ))}
+    const responseJson = await response.json();
+
+    console.log('responseJson', responseJson)
+  }
+
+  return <div className="flex flex-col h-screen">
+    <nav className="shadow px-4 py-2  felx flex-row justify-between items-center">
+      <div>Jobot</div>
+      <div>
+        <input 
+        type="password"
+        className="border p-2 rounded"
+        onChange = {e => setApikey(e.target.value)}
+        value={apiKey}
+        placeholder="Paste API Key here" />
       </div>
-      <div className="mt-2">
-        <input
-          type="text"
-          placeholder="Type your message here"
-          className="px-2 py-1 rounded-lg border-gray-300 w-full"
-        />
-        <button
-          onClick={handleMessageSend}
-          className="bg-blue-500 text-white px-4 py-1 rounded-lg ml-2"
-        >
-          Send
-        </button>
-      </div>
+    </nav>
+    <div className="p-4">
+      <button
+      className="border rounded-md p-2 bg-blue-500 hover:bg-blue-600"
+      onClick={sendRequest}>
+        Send Request
+      </button>
     </div>
-  );
-};
-
-export default function ChatPage() {
-  return (
-    <div className="flex flex-col h-screen">
-      <div className="flex-grow">
-        {/* Render other components here */}
-      </div>
-      <div className="px-4 py-2 bg-gray-200">
-        <ChatBox />
-      </div>
-    </div>
-  );
+  </div>
 }
